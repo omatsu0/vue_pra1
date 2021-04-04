dd14e8944087f096a098cb951ce2cfea9b6e5cb6
“omatsu0“ <your@example.com>
2021-04-04 22:21:47 +0900

    add routes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,85 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul class="links">
+      <li>
+        <router-link to="/" exact>HOME</router-link>
+      </li>
+      <li>
+        <router-link to="/about">ABOUT</router-link>
+      </li>
+      <li>
+        <router-link to="/detail">DETAIL</router-link>
+      </li>
+      <li>
+        <router-link to="/nest">NEST</router-link>
+      </li>
+    </ul>
+    <div class="page">
+      <transition name="router">
+        <router-view/>
+      </transition>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+};
 </script>
 
-<style lang="scss">
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
+}
+
+.links {
+  border-top: 1px solid #ddd;
+  border-bottom: 1px solid #ddd;
+  display: flex;
+  list-style: none;
+}
+
+.links > li > a {
+  display: block;
+  color: #666;
+  padding: 5px 20px;
+  text-decoration: none;
+  transition: background 0.4s;
+}
+.links > li > a:hover {
+  background: #f6f6f6;
+}
+.links > li > a.router-link-active {
+  background: transparent;
+  color: teal;
+  cursor: default;
+}
+
+.page {
+  position: relative;
+}
+.page > * {
+  position: absolute;
+}
+
+.router-enter-active {
+  transition: all 1s;
+  transition-delay: 0.5s;
+}
+.router-leave-active {
+  transition: all 0.5s;
+}
+.router-enter,
+.router-leave-to {
+  opacity: 0;
 }
 </style>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,14 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Router from "vue-router"
+
+import Index from "./components/pages/index";
+import About from "./components/pages/about";
+import Detail from "./components/pages/detail/index";
+import DetailId from "./components/pages/detail/_id";
+import Nest from "./components/pages/nest/parent";
+import Child1 from "./components/pages/nest/child1";
+import Child2 from "./components/pages/nest/child2";
+import Child3 from "./components/pages/nest/child3";
 
 Vue.use(VueRouter)
 
@@ -9,16 +18,36 @@ routes: [
   {
     path: '/',
     component: Index,
-    name: 'index',
   },
   {
     path: '/about',
     component: About,
-    name: 'about',
+  },
+  {
+    path: '/detail',
+    component: Detail
   },
   {
     path: '/detail/:id',
     component: DetailId
+  },
+  {
+    path: "/nest",
+    component: Nest,
+    children: [
+      {
+        path: "",
+        component: Child1
+      },
+      {
+        path: "child2",
+        component: Child2
+      },
+      {
+        path: "child3",
+        component: Child3
+      }
+    ]
   }
 ] 
 })
