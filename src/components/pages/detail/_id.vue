f10cb39c5d3a004819e6bf2c5d9e3506b62ea561
“omatsu0“ <your@example.com>
2021-04-05 22:29:25 +0900

    add detail_id.vue

---
+++ src/components/pages/detail/_id.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>This is Details Page"</h1>
+    <h2>ID ：{{$route.params.id}}</h2>
+  </div>
+</template>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
-import Vue from 'vue'
-import Router from "vue-router"
+import Vue from "vue";
+import Router from "vue-router";
 
 import Index from "./components/pages/index";
 import About from "./components/pages/about";
@@ -10,44 +10,44 @@ import Child1 from "./components/pages/nest/child1";
 import Child2 from "./components/pages/nest/child2";
 import Child3 from "./components/pages/nest/child3";
 
-Vue.use(VueRouter)
+Vue.use(Router);
 
-export default new VueRouter({
-mode: 'history',
-routes: [
-  {
-    path: '/',
-    component: Index,
-  },
-  {
-    path: '/about',
-    component: About,
-  },
-  {
-    path: '/detail',
-    component: Detail
-  },
-  {
-    path: '/detail/:id',
-    component: DetailId
-  },
-  {
-    path: "/nest",
-    component: Nest,
-    children: [
-      {
-        path: "",
-        component: Child1
-      },
-      {
-        path: "child2",
-        component: Child2
-      },
-      {
-        path: "child3",
-        component: Child3
-      }
-    ]
-  }
-] 
-})
+export default new Router({
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      component: Index
+    },
+    {
+      path: "/about",
+      component: About
+    },
+    {
+      path: "/detail",
+      component: Detail
+    },
+    {
+      path: "/detail/:id",
+      component: DetailId
+    },
+    {
+      path: "/nest",
+      component: Nest,
+      children: [
+        {
+          path: "",
+          component: Child1
+        },
+        {
+          path: "child2",
+          component: Child2
+        },
+        {
+          path: "child3",
+          component: Child3
+        }
+      ]
+    }
+  ]
+});
