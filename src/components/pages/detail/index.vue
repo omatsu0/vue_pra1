5f9122a9c2fb68715192da51b8a49b197a891645
“omatsu0“ <your@example.com>
2021-04-11 00:32:29 +0900

    add details_index.vue

---
+++ src/components/pages/detail/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>This is Detail Page!!</h1>
+    <ul>
+      <li class="detail-item" v-for="l in list" :key="l.id" @click="move(l.id)">
+        <span>{{l.id}} {{l.name}}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      list: [
+        { id: "001", name: "Aさん" },
+        { id: "002", name: "Bさん" },
+        { id: "003", name: "Cさん" },
+        { id: "004", name: "Dさん" },
+        { id: "005", name: "Eさん" }
+      ]
+    };
+  },
+  methods: {
+    move(id) {
+      this.$router.push(`/detail/${id}`);
+    }
+  }
+};
+</script>
